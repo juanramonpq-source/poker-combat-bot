0969f40aabbbd136f69e6a422807a834a8bb5704
Juan Ramon Perez Quintanar <juanramonperezquintanar@Host2011.local>
2026-06-01 21:36:00 +0200

    Publish clean PoCoBOT release 2026-06-01 19:33

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const http = require('http');
 const { Server } = require('socket.io');
 const crypto = require('crypto');
 
-const APP_RELEASE_VERSION = '20260601-mobile-shell-v1';
+const APP_RELEASE_VERSION = '20260601-mobile-shell-v2';
 const MAIN_GAME_PATH = '/poker_combat_bot_ONLINE.html';
 
 const app = express();
