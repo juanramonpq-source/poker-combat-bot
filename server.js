20d1aba4e92cc954748fe7cda62c766a8bedd128
Juan Ramon Perez Quintanar <juanramonperezquintanar@Host2011.local>
2026-09-06 13:15:16 +0200

    Publish clean PoCoBOT release 2026-09-06 11:15

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const http = require('http');
 const { Server } = require('socket.io');
 const crypto = require('crypto');
 
-const APP_RELEASE_VERSION = '20260709-drawer-tutobot-avatar-1';
+const APP_RELEASE_VERSION = '20260906-tutorial-two-phases-3';
 const MAIN_GAME_PATH = '/poker_combat_bot_ONLINE.html';
 
 const app = express();
